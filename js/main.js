66861c900dba6046c427421e80d40c22e4eaaaed
sunnatbee <sunnatbekyusupov@icloud.com>
2022-07-29 19:02:02 +0500

    Add rows

--- js/main.js
+++ js/main.js
@@ -1,33 +1,7 @@
-let elopen = document.querySelectorAll(".directors-inner-list__top")
-let close = document.querySelectorAll(".open-close")
-let elremove = document.querySelectorAll(".open-close-bottom")
-let elFather = document.querySelectorAll(".directors-inner-list__bottom")
+var elRotationBtns = document.querySelectorAll(".item__button");
 
-close.forEach(element => {
-    element.addEventListener("click", function(){
-        elopen.forEach(elm => {
-            elm.classList.remove("open")
-        })
-
-        element.parentElement.classList.add("open")
-
-        elFather.forEach(elm => {
-            elm.classList.remove("open")
-        })
+elRotationBtns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        btn.parentElement.classList.toggle("item__wrapper--active");
     })
-});
-
-elremove.forEach(closewindow => {
-    closewindow.addEventListener("click", function(){
-        elopen.forEach(elm => {
-            elm.classList.remove("open")
-        })
-        closewindow.parentElement.classList.add("open")
-
-        elopen.forEach(elm => {
-            elm.classList.remove("open")
-        })
-    })
-});
-console.log(elopen)
-console.log(elFather)
+})
